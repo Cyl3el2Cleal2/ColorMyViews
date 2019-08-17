52d8def97d3c2733b70aed06bbddbbbb1ef90bd7
cyl3el2cleal2 <nathachian@gmail.com>
2019-08-17 23:15:11 +0700

    Task 3

---
+++ app/src/main/java/buu/s59160937/colormyviews/MainActivity.kt
@@ -0,0 +1,12 @@
+package buu.s59160937.colormyviews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
